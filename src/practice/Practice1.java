9bf4866cbe285ee276f4dd482b68f802659f1972
Enlightment <B3DS-D01@users.noreply.github.com>
2019-02-09 11:53:09 +0530

    Update Practice1.java

--- src/practice/Practice1.java
+++ src/practice/Practice1.java
@@ -5,7 +5,13 @@ public class Practice1 {
 	public static void main(String[] args) {
 		p1();
 	}
-	
+	/*
+	* This function is created by vivek
+	*/
+	public static vivek()
+	{
+		System.out.println("I am vivek");
+	}
 	static void p1()
 	{
 		for (int i = 0;i <=20;i++)
